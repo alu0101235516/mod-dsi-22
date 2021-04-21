bdc5dceaf4b880b3e06e4138e3ad35da61355cff
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-04-22 00:51:29 +0100

    Creación del proyecto inicial

---
+++ src/prueba.ts
@@ -0,0 +1,11 @@
+/**
+ * Funcion para hacer una suma
+ * @param a valor 1
+ * @param b valor 2
+ * @returns suma de a y b
+ */
+function suma(a: number, b: number) {
+  return a + b;
+}
+
+console.log(suma(2, 2));
